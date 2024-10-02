c90363aae436652b06734ddec47f65f0dd4d65a4
DialloAbdourahman <dialliabdourahman78@gmail.com>
2024-10-02 13:47:44 +0100

    Updates

--- src/events/utiles.ts
+++ src/events/utiles.ts
@@ -3,11 +3,14 @@ export enum KEYS {
   TICKET_UPDATED = "ticket.updated",
   ORDER_CREATED = "order.created",
   ORDER_CANCELLED = "order.cancelled",
+  EXPIRATION_COMPLETE = "expiration.complete",
 }
 
 export enum QUEUES {
   TICKETING_QUEUE = "ticketing_queue",
   ORDERS_QUEUE = "orders_queue",
+  EXPIRATION_QUEUE = "expiration_queue",
+  PAYMENT_QUEUE = "payment_queue",
 }
 
 export enum EXCHANGES {
